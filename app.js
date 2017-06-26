892a0656df082ae7457369e5feb73dddc5d26d4e
byanofsky <byanofsky@me.com>
2017-06-26 15:50:42 -0700

    Add semi colons to app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-app.use(express.static('public'))
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
