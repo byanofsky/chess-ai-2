518c5904419e2dc4e1baa90669e7a0913cd70473
byanofsky <byanofsky@me.com>
2017-06-26 14:03:34 -0700

    Set up basic node server

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+app.use(express.static('public'))
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
