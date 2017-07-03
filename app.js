55b3ae8334982f8944e0ba123e301683dfd2aa0f
byanofsky <byanofsky@me.com>
2017-07-03 10:09:25 -0700

    Update port to work in Heroku

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
