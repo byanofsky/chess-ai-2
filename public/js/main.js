b68ba5635219b80987c7d7ed1152d8d9b3335a93
byanofsky <byanofsky@me.com>
2017-07-03 17:41:59 -0700

    Add interface for choosing which move algorithm to use

--- public/js/main.js
+++ public/js/main.js
@@ -53,18 +53,23 @@ var cfg = {
 }
 board = ChessBoard('board', cfg);
 
-// Computer makes a move with skill/depth level
-var makeMove = function(skill=3) {
+// Computer makes a move with algorithm choice and skill/depth level
+var makeMove = function(algo, skill=3) {
   // exit if the game is over
   if (game.game_over() === true) {
     console.log('game over');
     return;
   }
-  // Calculate the best move
-  // var move = randomMove();
-  // var move = calcBestMoveOne(game.turn());
-  var move = calcBestMoveNoAB(skill, game, game.turn())[1];
-  // var move = calcBestMove(skill, game, game.turn())[1];
+  // Calculate the best move, using chosen algorithm
+  if (algo === 1) {
+    var move = randomMove();
+  } else if (algo === 2) {
+    var move = calcBestMoveOne(game.turn());
+  } else if (algo === 3) {
+    var move = calcBestMoveNoAB(skill, game, game.turn())[1];
+  } else {
+    var move = calcBestMove(skill, game, game.turn())[1];
+  }
   // Make the calculated move
   game.move(move);
   // Update board positions
@@ -72,14 +77,14 @@ var makeMove = function(skill=3) {
 }
 
 // Computer vs Computer
-var playGame = function(skillW=2, skillB=2) {
+var playGame = function(algo=4, skillW=2, skillB=2) {
   if (game.game_over() === true) {
     console.log('game over');
     return;
   }
   var skill = game.turn() === 'w' ? skillW : skillB;
-  makeMove(skill);
+  makeMove(algo, skill);
   window.setTimeout(function() {
-    playGame(skillW, skillB);
+    playGame(algo, skillW, skillB);
   }, 250);
 };
